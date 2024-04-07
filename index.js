2b9065e2d47005c9c48180b4a1f66395f7068e04
parshantvijay <parshantdadlani28@gmail.com>
2024-04-07 23:36:51 +0500

    edit structure of a node application in backend server

---
+++ index.js
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Connect to DB and start server
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
