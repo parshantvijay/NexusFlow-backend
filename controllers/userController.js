39fb1fb5e73ac6466b76f5e78e2497bb9cd77fe7
Parshant Vijay <55455363+parshantvijay@users.noreply.github.com>
2024-05-13 08:36:54 +0500

    fixed an error

--- controllers/userController.js
+++ controllers/userController.js
@@ -272,7 +272,7 @@ const forgotPassword = asyncHandler(async (req, res) => {
       <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
 
       <p>Regards...</p>
-      <p>Pinvent Team</p>
+      <p>The NexusFlow Team</p>
     `;
   const subject = "Password Reset Request";
   const send_to = user.email;
